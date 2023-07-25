0ab1ca8309ff73afbb9d6b28d1ba6753bd84b1db
jtpheonix13 <jgtihunt@comcast.net>
2023-07-25 00:51:54 -0500

    updated SignUpForm.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,53 +1,44 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
-const SignupForm = () => {
-  // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-  // set state for form validation
-  const [validated] = useState(false);
-  // set state for alert
-  const [showAlert, setShowAlert] = useState(false);
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-  };
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
-    try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
-    setUserFormData({
+  const SignupForm = () => {
+    const [validated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    const [formState, setFormState] = useState({
       username: '',
       email: '',
       password: '',
     });
-  };
+
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+
+      setFormState({
+        ...formState,
+        [name]: value,
+      });
+    };
+
+    const [addUser, { error }] = useMutation(ADD_USER);
+
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+
+      try {
+        const { data } = await addUser({
+          variables: { ...formState},
+        });
+        Auth.login(data.addUser.token);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  
 
   return (
     <>
@@ -64,8 +55,8 @@ const SignupForm = () => {
             type='text'
             placeholder='Your username'
             name='username'
-            onChange={handleInputChange}
-            value={userFormData.username}
+            onChange={handleChange}
+            value={formState.username}
             required
           />
           <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
@@ -77,8 +68,8 @@ const SignupForm = () => {
             type='email'
             placeholder='Your email address'
             name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
+            onChange={handleChange}
+            value={formState.email}
             required
           />
           <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
@@ -90,18 +81,19 @@ const SignupForm = () => {
             type='password'
             placeholder='Your password'
             name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
+            onChange={handleChange}
+            value={formState.password}
             required
           />
           <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
         </Form.Group>
         <Button
-          disabled={!(userFormData.username && userFormData.email && userFormData.password)}
+          disabled={!(formState.username && formState.email && formState.password)}
           type='submit'
           variant='success'>
           Submit
         </Button>
+        {error && <div>Sign up Failed</div>}
       </Form>
     </>
   );
